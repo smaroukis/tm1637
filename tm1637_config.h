34158a6a8f9076b0b3519e01e23b3d4b822cec13
Spencer <smaroukis@gmail.com>
2024-07-13 19:44:45 +0900

    change bit delay for use with 3V3

--- tm1637_config.h
+++ tm1637_config.h
@@ -2,6 +2,7 @@
 #define _TM1637_CONFIG_H_
 
 #define _TM1637_FREERTOS            0
-#define _TM1637_BIT_DELAY           10
+#define _TM1637_BIT_DELAY           40
+// need bit_delay >= 40 for 3.3V supply
 
 #endif
